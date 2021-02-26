24baece51680d00a5dfbbd97f3b34301886a75ee
martinstaebler <mstaebler@perfectblue.de>
2021-02-26 23:16:43 +0100

    serverjs bearbeitet

---
+++ api/routes/climbRoutes.js
@@ -0,0 +1,14 @@
+const climbBuilder = require('../controllers/climbController');
+
+module.exports = app => {
+    app
+        .route('climbs')
+        .get(climbBuilder.zeige_alle_climbs)
+        .post(climbBuilder.erstelle_climb);
+
+    app
+        .route('/climbs/:climbId')
+        .get(climbBuilder.zeige_alle_climbs)
+        .put(climbBuilder.aktualisiere_climb)
+        .delete(climbBuilder.loesche_Climb);
+};
