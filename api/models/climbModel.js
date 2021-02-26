acd9799e863fbfca5aaf88eebdbfbbb5ccd31b55
martinstaebler <mstaebler@perfectblue.de>
2021-02-26 22:53:18 +0100

    climbSchema erstellt

---
+++ api/models/climbModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const climbSchema = new Schema(
+    {
+        nummer: {
+            type: String,
+            required: 'Routennummer darf nicht leer sein'
+        },
+        name: {
+            type: String,
+            required: 'Routenname darf nicht leer sein'
+        },
+        grad: {
+            type: String,
+            required: 'Schwierigkeitsgrad darf nicht leer sein'
+        },
+        datum: {
+            type: String,
+            required: 'Datum darf nicht leer sein'
+        },
+        routenbauer: {
+            type: String,
+            required: 'Routenbauer darf nicht leer sein'
+        },
+        sektor: {
+            type: String,
+            required: 'Sektor darf nicht leer sein'
+        }
+    },
+    { collection: 'climb'}
+);
