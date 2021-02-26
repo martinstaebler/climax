f03b74190fe7fa00f49b5fd12358787f42265190
martinstaebler <mstaebler@perfectblue.de>
2021-02-26 22:40:07 +0100

    Initial commit

---
+++ api/controllers/climbController.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const climb = mongoose.model('climb');
+
+exports.zeige_alle_climbs = (req, res) => {
+    climb.find({}, (err, climbs) => {
+        if (err) res.send(err);
+        res.json(climbs);
+    });
+};
+
+exports.erstelle_climb = (req, res) => {
+    const newClimb = new climb(req.body);
+    newClimb.save((err, climb) => {
+        if (err) res.send(err);
+        res.json(climbs);
+    });
+};
+
+exports.lese_climb = (req, res) => {
+    climb.findById(req.params.climbId, (err, climb) => {
+        if (err) res.send(err);
+        res.json(climb);
+    });
+};
+
+exports.aktualisiere_climb = (req, res) => {
+    climb.findOneAndUpdate(
+        { _id: req.params.climbId },
+        req.body,
+        { new: true },
+        (err, climb) => {
+            if (err) res.send(err);
+            res.json(climb);
+        }
+    );
+};
+
+exports.loesche_Climb = (req, res) => {
+    climb.deleteOne({ _id: req.params.climbId }, err => {
+        if (err) res.semd(err);
+        res.json({
+            message: 'Climb erfolgreich geloescht',
+            _id: req.params.climbId
+        });
+    });
+};
