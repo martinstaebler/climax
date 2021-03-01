97f7b94a166b80b0b7bf635adb7ab12dc0e8da24
martinstaebler <mstaebler@perfectblue.de>
2021-03-01 13:47:28 +0100

    ClimbAendern erstellt

---
+++ vueclimax/src/views/ClimbAendern.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="u-full-width">
+        <h3>Neue Route erstellen</h3>
+        <climb-formular @createOrUpdate="createOrUpdate"></climb-formular>
+    </div>
+</template>
+
+<script>
+    import climbFormular from '../components/climbFormular.vue';
+    import { api } from '../helpers/Helpers.js';
+
+    export default {
+        name: 'climb-aendern',
+        components: {
+            'climb-formular': climbFormular
+        },
+        data: function() {
+            return {
+                climb: {}
+            };
+        },
+        methods: {
+            createOrUpdate: async function(climb) {
+                await api.aktualisiereClimb(climb);
+                this.flash('Climb erfolgreich aktualisiert!', 'success');
+                this.$router.push(`/climbs/${climb._id}`);
+            }
+        },
+        async mounted() {
+            this.climb = await api.getClimb(this.$route.params.id);
+        }
+    }
+</script>
